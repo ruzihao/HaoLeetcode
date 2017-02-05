c6c83dbde69b9f38a134afa957e2a8e0e231a7f9
Hao Zhong <zhhao31@gmail.com>
2017-02-05 16:58:29 -0500

    failed case

---
+++ m20170205/pow_x_n.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def myPow(self, x, n):
+        """
+        :type x: float
+        :type n: int
+        :rtype: float
+        """
+        if n<0:
+            x = 1/x if x!=0 else 0
+        elif n==0:
+            return 1
+        else:
+            x = 1
+            for k in range(n):
+                x *= x
