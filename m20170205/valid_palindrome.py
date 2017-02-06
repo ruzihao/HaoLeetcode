cb88c6868171227de5c75f0b77d3075ae5d9724a
Hao Zhong <zhhao31@gmail.com>
2017-02-06 09:26:33 -0500

    done

--- m20170205/valid_palindrome.py
+++ m20170205/valid_palindrome.py
@@ -5,14 +5,17 @@ class Solution(object):
         :rtype: bool
         """
         valid_set = 'abcdefghijklmnopqrstuvwxyz' + '0123456789'
+        s = s.lower()
         ix, jx = 0, len(s)-1
         while ix<=jx:
-            if s[ix] != s[jx]:
-                return False
             if s[ix] not in valid_set:
                 ix += 1
                 continue
             if s[jx] not in valid_set:
                 jx -= 1
                 continue
+            if s[ix] != s[jx]:
+                return False
+            ix += 1
+            jx -= 1
         return True
