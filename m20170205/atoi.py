aefad8e4df359ac6267e4e42b841eeee64687944
Hao Zhong <zhhao31@gmail.com>
2017-02-05 21:42:12 -0500

    done

--- m20170205/atoi.py
+++ m20170205/atoi.py
@@ -4,6 +4,8 @@ class Solution(object):
         :type str: str
         :rtype: int
         """
+        max_int = 2147483647
+        min_int = -2147483648
         str = str.strip()
         if not len(str):
             return 0
@@ -20,6 +22,9 @@ class Solution(object):
         for ix, k in enumerate(str):
             if not k.isdigit():
                 break
+            if pos and ((res==max_int/10 and int(k)>7) or res>max_int/10):
+                return max_int
+            if not pos and ((res==max_int/10 and int(k)>8) or res>max_int/10):
+                return min_int
             res = res*10 + int(k)
         return res if pos else -res
-                
