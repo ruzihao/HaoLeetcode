eb09cf3c1efa7cf7149e269441abfcf4cee8ab96
Hao Zhong <zhhao31@gmail.com>
2017-02-07 08:29:45 -0500

    first attempt

---
+++ m20170205/plus_one.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def plusOne(self, digits):
+        """
+        :type digits: List[int]
+        :rtype: List[int]
+        """
+        carr = 1
+        for ix in range(len(digits))[::-1]:
+            if digits[ix] != 9 and carr:
+                digits[ix] += carr
+                carr = 0
+                break
+        if carr:
+            return [1] + digits
+        else:
+            return digits
