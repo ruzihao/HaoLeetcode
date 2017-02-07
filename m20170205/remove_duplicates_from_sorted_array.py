fcc2cee4128ef9dddb6210dd21600b0ae64692eb
Hao Zhong <zhhao31@gmail.com>
2017-02-07 01:09:26 -0500

    first attempt

---
+++ m20170205/remove_duplicates_from_sorted_array.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def removeDuplicates(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        if len(nums)<=1:
+            return len(nums)
+        else:
+            ix, jx = 0, 0
+            while jx<len(nums):
+                if nums[ix] == nums[jx]:
+                    jx += 1
+                else: 
+                    if ix+1 != jx:
+                        nums[ix+1] = nums[jx]
+                    ix += 1
+                    jx += 1
+            return ix+1
